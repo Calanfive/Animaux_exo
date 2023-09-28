0b93ddbbcce7d9241635c564b88a77b230f73bcf
calanfive <camille.lelan@gmail.com>
2023-09-28 14:53:32 +0200

    +interface_+abstracClass

---
+++ src/Animaux.ts
@@ -0,0 +1,4 @@
+export interface Ianimaux {
+    nom : string;
+    cri() : string    
+}
---
+++ src/Mammiferes.ts
@@ -0,0 +1,19 @@
+import { Ianimaux } from "./Animaux";
+
+export abstract class Mammiferes implements Ianimaux {
+    private _temps_Gestation : number;
+    public nom: string;
+    
+
+
+    constructor (nomAnim: string, temps_Gest: number) {
+        this.nom = nomAnim,
+        this._temps_Gestation = temps_Gest
+    }
+    
+    cri(): string {
+        throw new Error("Method not implemented.");
+    }
+
+    
+}
